f1a5d942ad773719400f4027090b31cd43393a51
Letycia Pinheiro <56917549+letyciap@users.noreply.github.com>
2021-02-10 20:28:58 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+echo "Atividade 2 de PSI"
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>atividade psi</title>
+</head>
+<body>
+<img src="https://www.hypeness.com.br/1/2019/07/golden-retriever-pelu%CC%81cia-1-1.png">
+
+</body>
+</html>
+
+?>
